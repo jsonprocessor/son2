247d957b33b15b1a0795b861b07c19115d0307f8
Kamil Adam <kamil.adam.zabinski@gmail.com>
2017-03-26 12:59:55 +0200

    new tests

---
+++ son2-main/src/test/scala/pl/writeonly/son2/core/CsvWord.scala
@@ -0,0 +1,31 @@
+package pl.writeonly.son2.core
+
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.JsonMappingException
+import org.scalatest.{Matchers, WordSpec}
+import pl.writeonly.son2.core.liners.{Liner, LinerOpt}
+import pl.writeonly.son2.core.providers.{Provider, ProviderCsv}
+
+class CsvWord extends WordSpec with Matchers {
+
+  val provider: Provider = new ProviderCsv()
+  "A Provider" should {
+    "produce JsonParseException when convert a" in {
+      assertThrows[JsonParseException] {
+        provider.convert("a")
+      }
+    }
+    "produce JsonMappingException when convert empty string" in {
+      assertThrows[JsonMappingException] {
+        provider.convert("")
+      }
+    }
+  }
+
+  val liner: Liner = new LinerOpt(provider)
+  "A Liner" should {
+    "return empty comment" in {
+      liner.apply("") should be(provider.comment("") + "\n")
+    }
+  }
+}
---
+++ son2-main/src/test/scala/pl/writeonly/son2/core/JavaPropsWord.scala
@@ -0,0 +1,31 @@
+package pl.writeonly.son2.core
+
+import com.fasterxml.jackson.core.JsonParseException
+import com.fasterxml.jackson.databind.JsonMappingException
+import org.scalatest.{Matchers, WordSpec}
+import pl.writeonly.son2.core.liners.{Liner, LinerOpt}
+import pl.writeonly.son2.core.providers.{Provider, ProviderJavaProps}
+
+class JavaPropsWord extends WordSpec with Matchers {
+
+  val provider: Provider = new ProviderJavaProps()
+  "A Provider" should {
+    "produce JsonParseException when convert a" in {
+      assertThrows[JsonParseException] {
+        provider.convert("a")
+      }
+    }
+    "produce JsonMappingException when convert empty string" in {
+      assertThrows[JsonMappingException] {
+        provider.convert("")
+      }
+    }
+  }
+
+  val liner: Liner = new LinerOpt(provider)
+  "A Liner" should {
+    "return empty comment" in {
+      liner.apply("") should be(provider.comment("") + "\n")
+    }
+  }
+}
