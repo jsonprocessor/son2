f735d2d21267677421c3797dcf6d5006fea42670
Kamil Adam <kamil.adam.zabinski@gmail.com>
2017-05-09 21:33:00 +0200

    coverage 0.01

--- son2-main/src/test/scala/pl/writeonly/son2/main/ResourceManagerWord.scala
+++ son2-main/src/test/scala/pl/writeonly/son2/main/ResourceManagerWord.scala
@@ -1,7 +1,8 @@
 package pl.writeonly.son2.main
 
+import pl.writeonly.son2.jack.main.Parser
 import pl.writeonly.son2.spec.WordCheckers
 
 class ResourceManagerWord extends WordCheckers {
-
+  val manager: ResourceManager = new ResourceManager
 }
