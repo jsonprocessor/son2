ce5c56124f5f0d15da8043254bd8d9ce46f80882
writeonly <enkidu616@gmail.com>
2017-05-11 12:20:01 +0200

    Update WordCheckers.scala

--- son2-spec/src/main/scala/pl/writeonly/son2/spec/WordCheckers.scala
+++ son2-spec/src/main/scala/pl/writeonly/son2/spec/WordCheckers.scala
@@ -1,9 +1,9 @@
 package pl.writeonly.son2.spec
 
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.WordSpec
-
 import org.scalatest.prop.Checkers
 
-class WordCheckers extends WordSpec with Checkers  {
+class WordCheckers extends WordSpec with MockFactory with Checkers  {
 
 }
