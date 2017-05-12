b3529cb718ee527962f7a4610e412a667e019ac1
writeonly <enkidu616@gmail.com>
2017-05-12 11:48:17 +0200

    Update StreamerSourceForeachWord.scala

--- son2-jack/src/test/scala/pl/writeonly/son2/jack/streamers/StreamerSourceForeachWord.scala
+++ son2-jack/src/test/scala/pl/writeonly/son2/jack/streamers/StreamerSourceForeachWord.scala
@@ -1,5 +1,7 @@
 package pl.writeonly.son2.jack.streamers
 
+import java.io.{PrintWriter, StringWriter}
+
 import pl.writeonly.son2.jack.providers.ProviderFake
 import pl.writeonly.son2.spec.WordFactory
 
@@ -8,16 +10,16 @@ import scala.io.Source
 class StreamerSourceForeachWord extends WordFactory {
 
   "A StreamerSourceForeach" when {
+    val streamer = new StreamerSourceForeach(new ProviderFake())
     "convert empty two lines to String" should {
       "return ?" in {
-        val streamer = new StreamerSourceForeach(new ProviderFake())
         streamer.source2string(Source.fromString("\n\n"))
       }
     }
     "convert empty two lines to pw" should {
       "return ?" in {
-        val streamer = new StreamerSourceForeach(new ProviderFake())
-        streamer.source2pw(Source.fromString("\n\n"), null)
+        val writer = new StringWriter()
+        streamer.source2pw(Source.fromString("\n\n"), new PrintWriter(writer))
       }
     }
   }
