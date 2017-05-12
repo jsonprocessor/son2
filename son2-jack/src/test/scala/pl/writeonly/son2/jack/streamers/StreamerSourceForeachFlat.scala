7a5dbaf7cf2290dbdc16baf767b1949e9f9db67f
writeonly <enkidu616@gmail.com>
2017-05-12 09:21:04 +0200

    Create StreamerSourceForeachFlat.scala

---
+++ son2-jack/src/test/scala/pl/writeonly/son2/jack/streamers/StreamerSourceForeachFlat.scala
@@ -0,0 +1,27 @@
+package pl.writeonly.son2.jack.streamers
+
+import java.io.File
+import java.net.URI
+
+import pl.writeonly.son2.jack.providers.ProviderFake
+import pl.writeonly.son2.spec.FlatFactory
+
+class StreamerSourceForeachFlat extends FlatFactory {
+
+  it should "produce NullPointerException when convertFile(File, File) is invoked" in {
+    assertThrows[NullPointerException] {
+      new StreamerSourceForeach(new ProviderFake()).convertFile(null.asInstanceOf[File], null.asInstanceOf[File])
+    }
+  }
+  it should "produce NullPointerException when convertFile(URL, URL) is invoked" in {
+    assertThrows[NullPointerException] {
+      new StreamerSourceForeach(new ProviderFake()).convertFile(null.asInstanceOf[URI], null.asInstanceOf[URI])
+    }
+  }
+  it should "produce NullPointerException when convertFile(String, String) is invoked" in {
+    assertThrows[NullPointerException] {
+      new StreamerSourceForeach(new ProviderFake()).convertFile(null.asInstanceOf[String], null.asInstanceOf[String])
+    }
+  }
+
+}
