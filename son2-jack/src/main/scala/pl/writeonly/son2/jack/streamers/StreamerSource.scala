3d88eae2ed922dc7a9704242fdef13f381085d5c
writeonly <enkidu616@gmail.com>
2017-05-12 15:02:23 +0200

    Update StreamerSource.scala

--- son2-jack/src/main/scala/pl/writeonly/son2/jack/streamers/StreamerSource.scala
+++ son2-jack/src/main/scala/pl/writeonly/son2/jack/streamers/StreamerSource.scala
@@ -3,8 +3,7 @@ package pl.writeonly.son2.jack.streamers
 import java.io.{File, InputStream, OutputStream, PrintWriter}
 import java.net.URI
 
-import pl.writeonly.son2.jack.liners.{Liner, LinerOpt}
-import pl.writeonly.son2.jack.providers.Provider
+import pl.writeonly.son2.jack.liners.Liner
 import pl.writeonly.son2.jack.util.Control
 import pl.writeonly.son2.jack.util.Control.using
 
@@ -16,23 +15,11 @@ abstract class StreamerSource(liner: Liner) extends Streamer(liner) {
 
   override def convertBytes(in: Array[Byte]): Array[Byte] = source2string(Source.fromRawBytes(in)).getBytes
 
-  override def convertFile(in: String, out: String): Unit = {
-    using(Source.fromFile(in)) { source =>
-      source2pw(source, new File(out))
-    }
-  }
+  override def convertFile(in: String, out: String): Unit = using(Source.fromFile(in))(s => source2pw(s, new File(out)))
 
-  override def convertFile(in: URI, out: URI): Unit = {
-    using(Source.fromFile(in)) { source =>
-      source2pw(source, new File(out))
-    }
-  }
+  override def convertFile(in: URI, out: URI): Unit = using(Source.fromFile(in))(s => source2pw(s, new File(out)))
 
-  override def convertFile(in: File, out: File): Unit = {
-    using(Source.fromFile(in)) { source =>
-      source2pw(source, out)
-    }
-  }
+  def source2pw(source: Source, out: File): Unit = using(new PrintWriter(out))(pw => source2pw(source, pw))
 
   override def convertStream(in: InputStream, out: OutputStream): Unit = {
     using(Source.fromInputStream(in, Control.UTF_8)) { source =>
@@ -42,11 +29,7 @@ abstract class StreamerSource(liner: Liner) extends Streamer(liner) {
     }
   }
 
-  def source2pw(source: Source, out: File): Unit = {
-    using(new PrintWriter(out)) { pw =>
-      source2pw(source, pw)
-    }
-  }
+  override def convertFile(in: File, out: File): Unit = using(Source.fromFile(in))(s => source2pw(s, out))
 
   def source2string(source: Source): String
 
