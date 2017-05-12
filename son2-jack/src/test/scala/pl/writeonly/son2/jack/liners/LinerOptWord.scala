5999da40351925da1bfb2c3a068bd75d50051075
writeonly <enkidu616@gmail.com>
2017-05-12 16:50:09 +0200

    Create LinerOptWord.scala

---
+++ son2-jack/src/test/scala/pl/writeonly/son2/jack/liners/LinerOptWord.scala
@@ -0,0 +1,21 @@
+package pl.writeonly.son2.jack.liners
+
+import pl.writeonly.son2.jack.core.Config
+import pl.writeonly.son2.jack.providers.Provider
+import pl.writeonly.son2.spec.WordFactory
+
+class LinerOptWord extends WordFactory {
+
+  "A LinerOpt" when {
+    "apply empty string" should {
+      "return ?" in {
+        class ProviderStub extends Provider(Config(), null)
+//        val provider: Provider = stub[ProviderStub]
+        val provider :Provider = new ProviderStub();
+        val liner = new LinerOpt(provider)
+        liner.apply("")
+      }
+    }
+  }
+
+}
