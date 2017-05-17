9bdfd0c5b66482ea137bc2310fb0b4c8a2c95aa7
writeonly <enkidu616@gmail.com>
2017-05-17 11:59:35 +0200

    Update Provider.scala

--- son2-jack/src/main/scala/pl/writeonly/son2/jack/providers/Provider.scala
+++ son2-jack/src/main/scala/pl/writeonly/son2/jack/providers/Provider.scala
@@ -4,10 +4,16 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import pl.writeonly.son2.jack.core.{Config, Converter, JsonNodeReader, JsonNodeWriter}
 import pl.writeonly.son2.jack.formats.MatcherFormatMapper
 
-class Provider(config: Config, out: ObjectMapper) {
+class Provider(val config: Config, out: ObjectMapper) {
   val converter = new Converter(new JsonNodeReader(MatcherFormatMapper(config)), new JsonNodeWriter(out, config.p))
 
   def convert(s: String) = converter.apply(s)
 
   def comment(s: String) = s
+
+  override def equals(obj: scala.Any): Boolean = obj.isInstanceOf[Provider] && config.equals(obj.asInstanceOf[Provider].config)
+
+  override def hashCode(): Int = config.hashCode()
+
+  override def toString: String =  " " + getClass + ", config " +  config.toString
 }
