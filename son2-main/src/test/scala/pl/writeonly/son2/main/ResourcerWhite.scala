5b10dd25e6886096200701c645935c9c24bdf541
writeonly <enkidu616@gmail.com>
2017-05-17 09:49:17 +0200

    Update ResourcerWhite.scala

--- son2-main/src/test/scala/pl/writeonly/son2/main/ResourcerWhite.scala
+++ son2-main/src/test/scala/pl/writeonly/son2/main/ResourcerWhite.scala
@@ -7,9 +7,19 @@ class ResourcerWhite extends WhiteSpec {
   val resourcer: Resourcer = new Resourcer(null)
 
   "A Resourcer" when {
-    "valid empty " should {
+    "valid empty string" should {
       "return README" in {
         assert(resourcer.valid("") ==  Resources.README)
+      }
+    }
+    "validOpt empty string" should {
+      "return README" in {
+        assert(resourcer.validOpt(Option("")) ==  Resources.README)
+      }
+    }
+    "validOpt empty option" should {
+      "return README" in {
+        assert(resourcer.validOpt(Option.empty[String]) ==  Resources.README)
       }
     }
   }
