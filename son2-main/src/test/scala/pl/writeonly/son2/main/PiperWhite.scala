27bd17c8aaec50dbc8c062b707f25cec8ae550d6
writeonly <enkidu616@gmail.com>
2017-05-17 16:25:55 +0200

    Create PiperWhite.scala

---
+++ son2-main/src/test/scala/pl/writeonly/son2/main/PiperWhite.scala
@@ -0,0 +1,23 @@
+package pl.writeonly.son2.main
+
+import java.io.ByteArrayOutputStream
+
+import pl.writeonly.son2.jack.core.Config
+import pl.writeonly.son2.jack.providers.ProviderFake
+import pl.writeonly.son2.jack.streamers.Streamer
+import pl.writeonly.son2.spec.WhiteSpec
+
+class PiperWhite extends WhiteSpec {
+  "A Piper" when {
+    "convertStream from empty string" should {
+      "return empty String" in {
+        val input = Streamer.toStream("")
+        val output = new ByteArrayOutputStream()
+        val params = Params(null, output)
+        val piper: Piper = new Piper(params, Config(), new ProviderFake())
+        piper.convertStream(input)
+        assert (Streamer.toString(output) == "")
+      }
+    }
+  }
+}
