4910ee567f79669cb55ae7f07cfd1f76dd25fcf5
writeonly <enkidu616@gmail.com>
2017-05-24 15:36:13 +0200

    Create MainerScalarSpec.scala

---
+++ son2-main/src/test/scala/pl/writeonly/son2/main/MainerScalarSpec.scala
@@ -0,0 +1,20 @@
+package pl.writeonly.son2.main
+
+import java.io.ByteArrayOutputStream
+
+import pl.writeonly.son2.jack.streamers.Streamer
+import pl.writeonly.son2.spec.GrayScalarSpec
+
+class MainerScalarSpec extends GrayScalarSpec {
+
+  describe("A Mainer") {
+    val input = Streamer.toStream("")
+    val output = new ByteArrayOutputStream()
+    val params = Params(input, output)
+
+    it("when format is 'HELP ME'") {
+      new Mainer(params, Array("HELP ME")).either
+    }
+
+  }
+}
