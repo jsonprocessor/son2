76e4bda1e4b7da40782245e63b98d98ed4adf0cb
writeonly <enkidu616@gmail.com>
2017-05-25 15:47:47 +0200

    Create ParamsResultSpec.scala

---
+++ son2-main/src/test/scala/pl/writeonly/son2/main/ParamsResultSpec.scala
@@ -0,0 +1,20 @@
+package pl.writeonly.son2.main
+
+import pl.writeonly.son2.spec.WhiteResultSpec
+
+class ParamsResultSpec extends WhiteResultSpec {
+  val params = Params(System.in, System.out)
+  val other = params
+  "A Params" when {
+    "equals other params" should {
+      "return true" in {
+        assertResult (params) (other)
+      }
+    }
+    "hashCode equals other params" should {
+      "return true" in {
+        assertResult (params.hashCode()) (other.hashCode())
+      }
+    }
+  }
+}
