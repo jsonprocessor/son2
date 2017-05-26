b6aaaf7becf4ff4d1561310ec3bab68416fcd228
writeonly <enkidu616@gmail.com>
2017-05-26 10:57:23 +0200

    Update WhiteResultSpec.scala

--- son2-spec/src/main/scala/pl/writeonly/son2/spec/WhiteResultSpec.scala
+++ son2-spec/src/main/scala/pl/writeonly/son2/spec/WhiteResultSpec.scala
@@ -1,8 +1,8 @@
 package pl.writeonly.son2.spec
 
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.WordSpec
+import org.scalatest.{PrivateMethodTester, WordSpec}
 
-abstract class WhiteResultSpec extends WordSpec with MockFactory {
+abstract class WhiteResultSpec extends WordSpec with PrivateMethodTester with MockFactory  {
 
 }
