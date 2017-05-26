7122a19c53254713202dade11e41dfb106146aed
writeonly <enkidu616@gmail.com>
2017-05-26 10:57:37 +0200

    Update WhiteThrowsSpec.scala

--- son2-spec/src/main/scala/pl/writeonly/son2/spec/WhiteThrowsSpec.scala
+++ son2-spec/src/main/scala/pl/writeonly/son2/spec/WhiteThrowsSpec.scala
@@ -1,9 +1,9 @@
 package pl.writeonly.son2.spec
 
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, PrivateMethodTester}
 import org.scalatest.prop.Checkers
 
-abstract class WhiteThrowsSpec extends FlatSpec with MockFactory with Checkers {
+abstract class WhiteThrowsSpec extends FlatSpec with PrivateMethodTester with MockFactory with Checkers {
 
 }
