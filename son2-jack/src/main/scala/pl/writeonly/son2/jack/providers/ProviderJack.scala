6d03e568e231e524e36cfbdc83c0087fe6718489
writeonly <enkidu616@gmail.com>
2017-06-02 10:33:57 +0200

    Update ProviderJack.scala

--- son2-jack/src/main/scala/pl/writeonly/son2/jack/providers/ProviderJack.scala
+++ son2-jack/src/main/scala/pl/writeonly/son2/jack/providers/ProviderJack.scala
@@ -1,13 +1,13 @@
 package pl.writeonly.son2.jack.providers
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import pl.writeonly.son2.core.converters.ConverterImpl
 import pl.writeonly.son2.core.providers.Provider
-import pl.writeonly.son2.jack.converters.ConverterJack
 import pl.writeonly.son2.jack.core.{Config, JsonNodeReader, JsonWriterJack}
 import pl.writeonly.son2.jack.formats.MatcherFormatMapper
 
 class ProviderJack(val config: Config, out: ObjectMapper)
-  extends Provider (new ConverterJack(new JsonNodeReader(MatcherFormatMapper(config)), new JsonWriterJack(out, config.p))) {
+  extends Provider (new ConverterImpl(new JsonNodeReader(MatcherFormatMapper(config)), new JsonWriterJack(out, config.p))) {
 
   override def equals(obj: scala.Any): Boolean = obj.isInstanceOf[ProviderJack] && config.equals(obj.asInstanceOf[ProviderJack].config)
 
