d4ee8ddc2b0ac9b0251dfa3ffb760ae5eb756999
writeonly <enkidu616@gmail.com>
2017-06-02 09:18:19 +0200

    Update ConverterJack.scala

--- son2-jack/src/main/scala/pl/writeonly/son2/jack/converters/ConverterJack.scala
+++ son2-jack/src/main/scala/pl/writeonly/son2/jack/converters/ConverterJack.scala
@@ -1,9 +1,9 @@
 package pl.writeonly.son2.jack.converters
 
 import pl.writeonly.son2.core.converters.Converter
-import pl.writeonly.son2.jack.core.{JsonNodeReader, JsonWriterJack}
+import pl.writeonly.son2.core.json.{JsonReader, JsonWriter}
 
-class ConverterJack(val in: JsonNodeReader, val out: JsonWriterJack) extends Converter {
+class ConverterJack(val in: JsonReader, val out: JsonWriter) extends Converter {
 
   def apply(content: String): String = out.write(in.readTree(content))
 
