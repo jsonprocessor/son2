4527edad2d8ae63fc5ee2b08a2b2d8762e508d67
writeonly <enkidu616@gmail.com>
2017-06-02 09:18:42 +0200

    Update JsonNodeReader.scala

--- son2-jack/src/main/scala/pl/writeonly/son2/jack/core/JsonNodeReader.scala
+++ son2-jack/src/main/scala/pl/writeonly/son2/jack/core/JsonNodeReader.scala
@@ -1,7 +1,8 @@
 package pl.writeonly.son2.jack.core
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import pl.writeonly.son2.core.json.JsonReader
 
-class JsonNodeReader(val mapper: ObjectMapper) {
-  def readTree(content: String) = mapper.readTree(content);
+class JsonNodeReader(val mapper: ObjectMapper) extends JsonReader {
+  def readTree(content: String) = mapper.readTree(content)
 }
