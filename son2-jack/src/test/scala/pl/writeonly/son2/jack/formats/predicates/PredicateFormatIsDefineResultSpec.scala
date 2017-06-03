cef8e9a3d243d3e104f705b516489858b421c465
Kamil Adam <kamil.adam.zabinski@gmail.com>
2017-06-03 19:03:17 +0200

    PredicateFormat

--- son2-jack/src/test/scala/pl/writeonly/son2/jack/formats/predicates/PredicateFormatIsDefineResultSpec.scala
+++ son2-jack/src/test/scala/pl/writeonly/son2/jack/formats/predicates/PredicateFormatIsDefineResultSpec.scala
@@ -1,52 +1,52 @@
-package pl.writeonly.son2.jack.formats.matchers
+package pl.writeonly.son2.jack.formats.predicates
 
 import pl.writeonly.son2.jack.formats.creators.CreatorFormatMapper
 import pl.writeonly.son2.spec.WhiteResultSpec
 
 
-class MatcherFormatIsDefineResultSpec extends WhiteResultSpec {
-  val matcher = new MatcherFormatIsDefine(new CreatorFormatMapper())
+class PredicatFormatIsDefineResultSpec extends WhiteResultSpec {
+  val predicate = new PredicateFormatIsDefine(new CreatorFormatMapper())
 
   "A Matcher" when {
     "param is empty object json" should {
       "return true" in {
         assertResult(true) {
-          matcher.on("{}")
+          predicate.on("{}")
         }
       }
     }
     "param is empty yaml" should {
       "return true" in {
         assertResult(true) {
-          matcher.yaml("---\n")
+          predicate.yaml("---\n")
         }
       }
     }
     "param is empty xml" should {
       "return true" in {
         assertResult(true) {
-          matcher.xml("<root />")
+          predicate.xml("<root />")
         }
       }
     }
     "param is empty csv" should {
       "return true" in {
         assertResult(true) {
-          matcher.csv("\n")
+          predicate.csv("\n")
         }
       }
     }
     "param is empty javaprops" should {
       "return true" in {
         assertResult(true) {
-          matcher.javaprops("")
+          predicate.javaprops("")
         }
       }
     }
     "param is empty properties" should {
       "return true" in {
         assertResult(true) {
-          matcher.properties("")
+          predicate.properties("")
         }
       }
     }
