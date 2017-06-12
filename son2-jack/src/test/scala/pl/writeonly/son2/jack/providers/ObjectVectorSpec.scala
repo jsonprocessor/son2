a7a72783441c8e39dda49f3eea56c333c653eb77
writeonly <enkidu616@gmail.com>
2017-06-12 15:51:00 +0200

    Create ObjectVectorSpec.scala

---
+++ son2-jack/src/test/scala/pl/writeonly/son2/jack/providers/ObjectVectorSpec.scala
@@ -0,0 +1,72 @@
+package pl.writeonly.son2.jack.providers
+
+import pl.writeonly.son2.core.liners.{Liner, LinerOpt}
+import pl.writeonly.son2.core.providers.Provider
+import pl.writeonly.son2.core.streamers.{Streamer, StreamerPipeForeach}
+import pl.writeonly.son2.jack.core.{Config, Formats}
+import pl.writeonly.son2.jack.glue.MatcherFormatProvider
+import pl.writeonly.son2.spec.GrayVectorSpec
+
+class ObjectVectorSpec extends GrayVectorSpec {
+
+  val toSuccess = Table(
+    ("in", "out"),
+    ("0", "0"),
+    ("\"a\"", "\"a\""),
+    ("[]", "[]"),
+    ("[0]", "[0]"),
+    ("[0,1]", "[0,1]"),
+    ("{}", "{}"),
+    ("{\"a\":0}", "{\"a\":0}"),
+    ("{\"a\":0,\"b\":1}", "{\"a\":0,\"b\":1}"),
+    ("[{}]", "[{}]"),
+    ("{\"a\":[]}", "{\"a\":[]}")
+  )
+
+  val toFailure = Table(
+    "in",
+    "a"
+  )
+
+  val provider: Provider = MatcherFormatProvider(Config(o = Formats.OBJECT, p = false))
+  property("convert son to yaml by provider") {
+    forAll(toSuccess) { (in, out) =>
+      provider.convert(in) should be(out)
+    }
+  }
+
+  val liner: Liner = new LinerOpt(provider)
+  property("convert son to yaml by liner") {
+    forAll(toSuccess) { (in, out) =>
+      liner.apply(in) should be(out + "\n")
+    }
+  }
+  property("fail convert son to yaml by liner") {
+    forAll(toFailure) { in =>
+      liner.apply(in) should be(provider.comment(in) + "\n")
+    }
+  }
+
+  val streamer: Streamer = new StreamerPipeForeach(liner)
+  property("convert son to yaml by streamer") {
+    forAll(toSuccess) { (in, out) =>
+      streamer.convertString(in) should be(out + "\n")
+    }
+  }
+  property("fail convert son to yaml by streamer") {
+    forAll(toFailure) { in =>
+      streamer.convertString(in) should be(provider.comment(in) + "\n")
+    }
+  }
+
+  property("convert son to yaml by native streamer") {
+    forAll(toSuccess) { (in, out) =>
+      streamer.convertStringNative(in) should be(out + "\n")
+    }
+  }
+  property("fail convert son to yaml by native streamer") {
+    forAll(toFailure) { in =>
+      streamer.convertStringNative(in) should be(provider.comment(in) + "\n")
+    }
+  }
+}
