e77837be72b0c20a42b419868e53f65f82820ba5
writeonly <enkidu616@gmail.com>
2017-06-13 11:14:59 +0200

    Create ConfigPath.scala

--- son2-path/src/main/scala/pl/writeonly/son2/path/glue/ConfigPath.scala
+++ son2-path/src/main/scala/pl/writeonly/son2/path/glue/ConfigPath.scala
@@ -3,7 +3,7 @@ package pl.writeonly.son2.path.glue
 import pl.writeonly.son2.core.glue.Config
 
 object ConfigPath {
-  def apply(i:String = "$", s : Boolean = true, o : String = "j", p : Boolean = false) = new Config(i, s, o, p)
+  def apply(i:String = "$", s : Boolean = true, o : String = "SMART", p : Boolean = false) = new Config(i, s, o, p)
 }
 
 
