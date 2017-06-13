eaa2959ca2f1674f931e1a961c55780212eba2d2
writeonly <enkidu616@gmail.com>
2017-06-13 12:04:22 +0200

    Create NotationReaderStrict.scala

---
+++ son2-path/src/main/scala/pl/writeonly/son2/path/notation/NotationReaderStrict.scala
@@ -0,0 +1,9 @@
+package pl.writeonly.son2.path.notation
+
+import net.minidev.json.JSONValue
+import pl.writeonly.son2.core.notation.NotationReader
+
+class NotationReaderStrict() extends NotationReader {
+  def apply(content: String):Any = JSONValue.parseStrict(content)
+  def isValid(content :String): Boolean = JSONValue.isValidJsonStrict(content)
+}
