9108931dd9940726ab93f01fccbe0d743101562e
writeonly <enkidu616@gmail.com>
2017-06-13 12:05:26 +0200

    Create MatcherFormatPath.scala

--- son2-path/src/main/scala/pl/writeonly/son2/path/formats/matchers/MatcherFormatPath.scala
+++ son2-path/src/main/scala/pl/writeonly/son2/path/formats/matchers/MatcherFormatPath.scala
@@ -2,17 +2,18 @@ package pl.writeonly.son2.path.formats.matchers
 
 import pl.writeonly.son2.core.formats.matchers.MatcherFormat
 import pl.writeonly.son2.core.notation.NotationReader
-import pl.writeonly.son2.path.notation.{NotationReaderPath, NotationReaderSmart}
+import pl.writeonly.son2.path.notation.{NotationReaderPath, NotationReaderSmart, NotationReaderStrict}
 
 class MatcherFormatPath() extends MatcherFormat[NotationReader] {
   def apply(s: String): Either[Option[String], NotationReader] = s match {
     case s if predicate(s) => Right(new NotationReaderPath(s))
-    case s if smart(s) => Right(new NotationReaderSmart(s))
+    case s if smart(s) => Right(new NotationReaderSmart())
+    case s if strict(s) => Right(new NotationReaderStrict())
     case s => left(s)
   }
 
   def predicate(s:String) = s.startsWith("$")
   def smart(s:String) = "SMART".startsWith(s)
-
+  def strict(s:String) = "STRICT".startsWith(s)
 
 }
