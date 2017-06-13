e318381d32b5be3172d0697465f70ac51b8cb38b
writeonly <enkidu616@gmail.com>
2017-06-13 12:04:40 +0200

    Update NotationReaderSmart.scala

--- son2-path/src/main/scala/pl/writeonly/son2/path/notation/NotationReaderSmart.scala
+++ son2-path/src/main/scala/pl/writeonly/son2/path/notation/NotationReaderSmart.scala
@@ -3,15 +3,7 @@ package pl.writeonly.son2.path.notation
 import net.minidev.json.JSONValue
 import pl.writeonly.son2.core.notation.NotationReader
 
-class NotationReaderSmart(val path: String) extends NotationReader {
-
+class NotationReaderSmart() extends NotationReader {
   def apply(content: String):Any = JSONValue.parse(content)
-
-  def parseKeepingOrder(content: String):Any = JSONValue.parseKeepingOrder(content)
-
-  def parseStrict(content: String):Any = JSONValue.parseStrict(content)
-
   def isValid(content :String): Boolean = JSONValue.isValidJson(content)
-
-  def isValidStrict(content :String): Boolean = JSONValue.isValidJson(content)
 }
