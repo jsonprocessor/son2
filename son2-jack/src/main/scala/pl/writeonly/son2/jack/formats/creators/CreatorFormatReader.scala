7048d9782cabb4cdccfb0f4ff0c2348ffb585aef
writeonly <enkidu616@gmail.com>
2017-07-03 13:14:16 +0200

    Update CreatorFormatReader.scala

--- son2-jack/src/main/scala/pl/writeonly/son2/jack/formats/creators/CreatorFormatReader.scala
+++ son2-jack/src/main/scala/pl/writeonly/son2/jack/formats/creators/CreatorFormatReader.scala
@@ -1,22 +1,17 @@
 package pl.writeonly.son2.jack.formats.creators
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.csv.CsvMapper
-import com.fasterxml.jackson.dataformat.javaprop.JavaPropsMapper
-import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
-import pl.writeonly.son2.jack.notation.NotationReaderJack
+import pl.writeonly.son2.jack.notation._
 
 class CreatorFormatReader extends CreatorFormat[NotationReaderJack] {
 
-  override def on: NotationReaderJack = new NotationReaderJack(new ObjectMapper)
+  override def on: NotationReaderJack = new NotationReaderObject
 
-  override def yaml: NotationReaderJack = new NotationReaderJack(new YAMLMapper)
+  override def yaml: NotationReaderJack = new NotationReaderYaml
 
-  override def xml: NotationReaderJack = new NotationReaderJack(new XmlMapper)
+  override def xml: NotationReaderJack = new NotationReaderXml
 
-  override def csv: NotationReaderJack = new NotationReaderJack(new CsvMapper)
+  override def csv: NotationReaderJack = new NotationReaderCsv
 
-  override def javaprops: NotationReaderJack = new NotationReaderJack(new JavaPropsMapper)
+  override def javaprops: NotationReaderJack = new NotationReaderJavaProps
 }
 
