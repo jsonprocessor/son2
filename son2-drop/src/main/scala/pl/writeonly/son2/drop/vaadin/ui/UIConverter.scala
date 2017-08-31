8b3c3c4888bf2d60c2e0c0eb17878ce161f67069
Kamil Adam <enkidu616@gmail.com>
2017-08-31 22:34:45 +0200

    UIUtil

--- son2-drop/src/main/scala/pl/writeonly/son2/drop/vaadin/ui/UIConverter.scala
+++ son2-drop/src/main/scala/pl/writeonly/son2/drop/vaadin/ui/UIConverter.scala
@@ -1,14 +1,16 @@
 package pl.writeonly.son2.drop.vaadin.ui
 
+import java.util.Optional
+
 import com.google.common.collect.ImmutableList
 import com.vaadin.annotations.{Theme, Title}
 import com.vaadin.ui.Button.ClickEvent
 import com.vaadin.ui._
 import pl.writeonly.son2.core.config.{Config, RConfig, WConfig}
+import pl.writeonly.son2.core.glue.Streamers
 import pl.writeonly.son2.jack.glue.CreatorProviderJack
 
 import scala.collection.JavaConverters._
-
 import pl.writeonly.son2.drop.vaadin.JavaFunctions._
 
 @Title("json converter")
@@ -20,6 +22,7 @@ class UIConverter extends UITrait {
 
   override def components: List[Component] = {
     val input = inputTextArea
+    val configLabel = outputLabel
     val output = outputLabel
 
     val formatsMapping = Map[String, Symbol]("JSON" -> 'object, "YAML" -> 'yaml, "XML" -> 'xml, "Java properties" -> 'propierties)
@@ -31,15 +34,21 @@ class UIConverter extends UITrait {
 
     val convert = convertButton(new Button.ClickListener() {
       override def buttonClick(clickEvent: ClickEvent): Unit = {
-        val inputFormat = inputFormats.getSelectedItem().map((it:String) => formatsMapping.get(it).get).orElse('object)
-        val outputFormat = inputFormats.getSelectedItem().map((it:String) => formatsMapping.get(it).get).orElse('yaml)
-        val config = Config(RConfig(format = inputFormat), WConfig(format = outputFormat))
+        val inputFormat :Optional[Symbol] = inputFormats.getSelectedItem().map((it:String) => formatsMapping.get(it).get)
+        val outputFormat :Optional[Symbol] = inputFormats.getSelectedItem().map((it:String) => formatsMapping.get(it).get)
+        val config = Config(
+          RConfig(format = inputFormat.orElse('object)),
+          WConfig(format = outputFormat.orElse('yaml))
+        )
+        configLabel.setValue(config.toString)
         val provider = CreatorProviderJack(config)
-        val value = provider.convert(input.getValue)
-        output.setValue(value)
+        val inputValue = input.getValue
+        val streamer = Streamers.pipe(false, provider)
+        val outputValue = streamer.convertStringNative(inputValue)
+        output.setValue(outputValue)
       }
     })
 
-    return List[Component](toBackLink, input, optionsPanel(components), convert, output)
+    return List[Component](toBackLink, input, optionsPanel(components), convert, configLabel, output)
   }
 }
