215d094a07159f18c5a2165d222f4ca3829b2c06
writeonly <enkidu616@gmail.com>
2017-09-05 12:30:41 +0200

    Update FormatsPath.scala

--- son2-path/src/main/scala/pl/writeonly/son2/path/core/FormatsPath.scala
+++ son2-path/src/main/scala/pl/writeonly/son2/path/core/FormatsPath.scala
@@ -1,9 +1,14 @@
 package pl.writeonly.son2.path.core
 
 object FormatsPath {
+  val GSON = 'gson
+  val JACKSON_NODE = 'node
+  val JACKSON = 'jackson
+  val JETTISON = 'jettison
+  val ORG = 'org
   val SMART = 'smart
   val STRICT = 'strict
-  val GSON = 'gson
-  val PATH = '$
+  val TAPESTRY = 'tapestry
 
+  val PATH = '$
 }
