c420faca457931b4d0fff9ea6b4d9d7f991fb156
writeonly <enkidu616@gmail.com>
2017-09-06 08:46:10 +0200

    Update UIComparator.scala

--- son2-drop/src/main/scala/pl/writeonly/son2/drop/vaadin/ui/UIComparator.scala
+++ son2-drop/src/main/scala/pl/writeonly/son2/drop/vaadin/ui/UIComparator.scala
@@ -3,51 +3,29 @@ package pl.writeonly.son2.drop.vaadin.ui
 import com.vaadin.annotations.{Theme, Title}
 import com.vaadin.ui.Button.ClickEvent
 import com.vaadin.ui._
-import org.json.JSONException
-import org.skyscreamer.jsonassert.{JSONAssert, JSONCompare, JSONCompareMode, JSONCompareResult}
+import com.vaadin.ui.themes.ValoTheme
+import pl.writeonly.son2.core.util.JsonComparator
 import pl.writeonly.son2.drop.vaadin.util.Mappings
-import pl.writeonly.son2.path.core.ConfigPath
-import pl.writeonly.son2.path.glue.CreatorConverterPath
 
-import scala.collection.JavaConverters._
-import scala.util.Try
-import scala.util.control.Exception.catching
-
-@Title("json path")
+@Title("json comparator")
 @Theme("valo")
 class UIComparator extends UITrait {
 
-//  val modeGroup =
-
-
   override def components: List[Component] = {
-    val checkBoxes = nativeGroup
-    val configLabel = outputLabel
     val inputLeft = inputTextArea("expected json:")
     val inputRight = inputTextArea("actual json:")
     val output = outputLabel
 
     val modeGroup = radioButtonGroup("Compare mode", Mappings.modeMapping)
-    val outputFormats = jacksonOutputFormat("JSON")
-    val components: List[Component] = List(modeGroup)
-
+    modeGroup.addStyleName(ValoTheme.OPTIONGROUP_HORIZONTAL)
 
     val convert = convertButton(new Button.ClickListener() {
       override def buttonClick(clickEvent: ClickEvent): Unit = {
         val mode = selectedItem(modeGroup, Mappings.modeMapping)
-//        JSONAssert.assertEquals(inputLeft.getValue, inputRight.getValue, mode)
-        val result: Either[Throwable, JSONCompareResult] = catching(classOf[Exception])
-          .either(JSONCompare.compareJSON(inputLeft.getValue, inputRight.getValue, mode))
-        val value = result match {
-          case Right(result) => result.toString
-          case Left(exception) => exception.getMessage
-        }
+        val value = JsonComparator(inputLeft.getValue, inputRight.getValue, mode)
         output.setValue(value)
-
-
       }
-    });
-
-    return List(linkPanel, optionsPanel(components), inputLeft, inputRight, convert, output)
+    })
+    return List(linkPanel, modeGroup, inputLeft, inputRight, convert, output)
   }
 }
