395c7585931512a5e6cb1c49f6be27027424e5ab
kamil-adam <kamil.zabinski@relayr.io>
2017-09-19 16:42:48 +0200

    ComplexSmartOptions

--- son2-drop/src/main/scala/pl/writeonly/son2/drop/vaadin/complexes/ComplexSmartOptions.scala
+++ son2-drop/src/main/scala/pl/writeonly/son2/drop/vaadin/complexes/ComplexSmartOptions.scala
@@ -15,12 +15,11 @@ class ComplexSmartOptions extends Complex with LazyLogging{
   private val vertical = ComplexSmartOptions.verticalPanel("Smart modes:", modes: _*)
   private val horizontal = ComplexSmartOptions.horizontalLayout(vertical, options)
 
-  private def button(caption:String, select: Set[Symbol]) = new Button(caption, new Button.ClickListener(){
+  private def button(caption:String, select: Set[ItemSymbol]) = new Button(caption, new Button.ClickListener(){
     override def buttonClick(event: ClickEvent): Unit = {
       logger.info("{}", event)
       options.clear()
-      val set : Set[String] = select.map(ComplexSmartOptions.swap.get(_).get)
-      options.select(set.toSeq:_*)
+      options.select(select.toSeq:_*)
     }
   })
 
@@ -30,54 +29,47 @@ class ComplexSmartOptions extends Complex with LazyLogging{
 }
 
 object ComplexSmartOptions extends UITrait {
+  private val ACCEPT_USELESS_COMMA =  ItemSymbol('ACCEPT_USELESS_COMMA)
+  private val USE_HI_PRECISION_FLOAT = ItemSymbol('USE_HI_PRECISION_FLOAT)
+  private val ACCEPT_TAILLING_DATA = ItemSymbol('ACCEPT_TAILLING_DATA)
+  private val ACCEPT_TAILLING_SPACE = ItemSymbol('ACCEPT_TAILLING_SPACE)
+  private val REJECT_127_CHAR = ItemSymbol('REJECT_127_CHAR)
+  private val USE_INTEGER_STORAGE = ItemSymbol('USE_INTEGER_STORAGE)
+
   private val items = Set(
     ItemSymbol('ACCEPT_SIMPLE_QUOTE),
     ItemSymbol('ACCEPT_NON_QUOTE),
     ItemSymbol('ACCEPT_NAN),
     ItemSymbol('IGNORE_CONTROL_CHAR),
-    ItemSymbol('USE_INTEGER_STORAGE),
     ItemSymbol('ACCEPT_LEADING_ZERO),
-    ItemSymbol('ACCEPT_USELESS_COMMA),
-    ItemSymbol('USE_HI_PRECISION_FLOAT),
-    ItemSymbol('ACCEPT_TAILLING_DATA),
-    ItemSymbol('ACCEPT_TAILLING_SPACE),
-    ItemSymbol('ACCEPT_USELESS_COMMA),
-    ItemSymbol('REJECT_127_CHAR)
+    USE_INTEGER_STORAGE,
+    ACCEPT_USELESS_COMMA,
+    USE_HI_PRECISION_FLOAT,
+    ACCEPT_TAILLING_DATA,
+    ACCEPT_TAILLING_SPACE,
+    REJECT_127_CHAR
   )
-  private val mapping = Map[String, Symbol](
-    "ACCEPT_SIMPLE_QUOTE" -> 'ACCEPT_SIMPLE_QUOTE,
-    "ACCEPT_NON_QUOTE" -> 'ACCEPT_NON_QUOTE,
-    "ACCEPT_NAN" -> 'ACCEPT_NAN,
-    "IGNORE_CONTROL_CHAR" -> 'IGNORE_CONTROL_CHAR,
-    "USE_INTEGER_STORAGE" -> 'USE_INTEGER_STORAGE,
-    "ACCEPT_LEADING_ZERO" -> 'ACCEPT_LEADING_ZERO,
-    "ACCEPT_USELESS_COMMA" -> 'ACCEPT_USELESS_COMMA,
-    "USE_HI_PRECISION_FLOAT" -> 'USE_HI_PRECISION_FLOAT,
-    "ACCEPT_TAILLING_DATA" -> 'ACCEPT_TAILLING_DATA,
-    "ACCEPT_TAILLING_SPACE" -> 'ACCEPT_TAILLING_SPACE,
-    "ACCEPT_USELESS_COMMA" -> 'ACCEPT_USELESS_COMMA,
-    "REJECT_127_CHAR" -> 'REJECT_127_CHAR
-  )
-  val MODE_PERMISSIVE: Set[Symbol] = items.map(_.value)
-  val MODE_JSON_SIMPLE: Set[Symbol] = Set(
-    'ACCEPT_USELESS_COMMA,
-    'USE_HI_PRECISION_FLOAT,
-    'ACCEPT_TAILLING_DATA,
-    'ACCEPT_TAILLING_SPACE,
-    'REJECT_127_CHAR)
-  val MODE_RFC4627: Set[Symbol] = Set(
-    'USE_INTEGER_STORAGE,
-    'USE_HI_PRECISION_FLOAT,
-    'ACCEPT_TAILLING_SPACE)
-  val MODE_STRICTEST: Set[Symbol] = Set(
-    'USE_INTEGER_STORAGE,
-    'USE_HI_PRECISION_FLOAT,
-    'REJECT_127_CHAR)
-  private val swap: Map[Symbol, String] = mapping.map(_.swap)
 
-  private def apply = checkBoxGroup("Smart options:", ComplexSmartOptions.mapping)
+  val MODE_PERMISSIVE: Set[ItemSymbol] = items
+  val MODE_JSON_SIMPLE: Set[ItemSymbol] = Set(
+    ACCEPT_USELESS_COMMA,
+    USE_HI_PRECISION_FLOAT,
+    ACCEPT_TAILLING_DATA,
+    ACCEPT_TAILLING_SPACE,
+    REJECT_127_CHAR)
+  val MODE_RFC4627: Set[ItemSymbol] = Set(
+    USE_INTEGER_STORAGE,
+    USE_HI_PRECISION_FLOAT,
+    ACCEPT_TAILLING_SPACE)
+  val MODE_STRICTEST: Set[ItemSymbol] = Set(
+    USE_INTEGER_STORAGE,
+    USE_HI_PRECISION_FLOAT,
+    REJECT_127_CHAR)
 
-  private def selectedItem(component: CheckBoxGroup[String]): Set[Symbol] = selectedItem(component, ComplexSmartOptions.mapping)
+
+  private def apply = checkBoxGroup2("Smart options:", items)
+
+  private def selectedItem(component: CheckBoxGroup[ItemSymbol]): Set[Symbol] = selectedItem2(component).map(_.value)
 
 
 
