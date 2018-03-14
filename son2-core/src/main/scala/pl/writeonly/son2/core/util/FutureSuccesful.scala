3a88a22a23c69eed52c23c509c22dd2eb72dfbd0
Kamil Zabinski <kamil.zabinski@relayr.io>
2018-03-14 08:15:38 +0100

    refactoring for scala 2.11

--- son2-core/src/main/scala/pl/writeonly/son2/core/util/FutureSuccesful.scala
+++ son2-core/src/main/scala/pl/writeonly/son2/core/util/FutureSuccesful.scala
@@ -9,28 +9,40 @@ object FutureSuccesful {
 
   def opt[A](value: Future[A])(
       implicit ec: ExecutionContext): Future[Option[A]] =
-    value.transform({
-      case Success(s) => Success(Option(s))
-      case Failure(_) => Success(None)
-    })
+//    value.transform({
+//      case Success(s) => Success(Option(s))
+//      case Failure(_) => Success(None)
+//    })
+    transform(value, (s: A) => Option(s), { case _ => None })
 
   def either[A](value: Future[A])(
       implicit ec: ExecutionContext): Future[Either[Throwable, A]] =
-    value.transform({
-      case Success(s) => Success(Right(s))
-      case Failure(t) => Success(Left(t))
-    })
+//    value.transform({
+//      case Success(s) => Success(Right(s))
+//      case Failure(t) => Success(Left(t))
+//    })
+    transform(value, (s: A) => Right(s), { case t => Left(t) })
 
   def tryWith[A](value: Future[A])(
       implicit ec: ExecutionContext): Future[Try[A]] =
-    value.transformWith(Future.successful)
+//    value.transformWith(Future.successful)
+    transform(value, (s: A) => Success(s), { case t => Failure(t) })
 
   def or[A](value: Future[A])(
       implicit ec: ExecutionContext): Future[A Or Throwable] =
-    value.transform({
-      case Success(s) => Success(Good(s))
-      case Failure(t) => Success(Bad(t))
-    })
+//    value.transform({
+//      case Success(s) => Success(Good(s))
+//      case Failure(t) => Success(Bad(t))
+//    })
+    transform(value, (s: A) => Good(s), { case t => Bad(t) })
+
+  private def transform[S, T](value: Future[T],
+                              s: T => S,
+                              pf: PartialFunction[Throwable, S])(
+      implicit executor: ExecutionContext): Future[S] =
+    value
+      .transform(s, e => e)
+      .recover(pf)
 
   implicit class FutureSuccessful[A](value: Future[A])(
       implicit ec: ExecutionContext) {
